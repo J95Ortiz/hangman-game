1b1815dec4fad5c373e900f5c96a2d3aa4364038
Joe Ortiz <Se_sema_95@hotmail.com>
2024-06-10 15:37:58 +0000

    Added some functions to code

--- run.py
+++ run.py
@@ -3,21 +3,36 @@
 # Importing random so the computer chooses a random word to use in the game
 import random
 
+# Setting up the names for word files
+easy = open("easy.txt", "r")
+########## medium = 
+########## hard = 
+########## mix = 
+
 # Fn asking for User's name, will add code to set up game
 def introduction():
     print("Hello!")
     name = input("What's your name? \n")
-    print(f'Hi {name}, did you want to have a game of Hangman?')
+    print(f'Welcome to the Hangman Game {name}! \nDid you want to have a game?')
+    
+    # Code requesting User chooses how hard the game should be
+    print("There are three difficulty levels you can choose from (Easy, Medium and Hard), or you can have a mix of them all. \n")
+    level_choice = input("Please choose a level now (Easy, Medium, Hard or Mix):\n")
+    # Turns input into lower case
+    level_aplied = level_choice.lower()
+    return level_aplied
+    print(level_aplied)
+
 
 
 # Fn which goes through chosen list and selects a word to use
-def word_choice():
-    with open("easy.txt", mode="r") as file:
-        word_selector = file.read().split("\n")
-    word = random.choice(word_selector)
-    print(word)
+def word_choice(level):
+    if level == easy:
+        word_selector = easy.read().split("\n")
+        word = random.choice(word_selector)
+    # return word
+        print(word)
 
-
-introduction()
-word_choice()
+level = introduction()
+word_choice(introduction)
 

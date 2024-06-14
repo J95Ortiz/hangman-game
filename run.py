8178e61f4866d33fd51473db61b46efc47a463c9
Joe Ortiz <Se_sema_95@hotmail.com>
2024-06-14 14:48:24 +0000

    Game pretty much complete, need to add minor changes

--- run.py
+++ run.py
@@ -5,11 +5,9 @@ import random
 
 # Setting up the names for word files
 easy = open("easy.txt", "r")
-#print(easy)
 medium = open("medium.txt", "r")
-#print(medium)
-########## hard = 
-########## mix = 
+hard = open("hard.txt", "r")
+mix = open("mix.txt", "r")
 
 
 # Fn asking for User's name, will add code to set up game
@@ -21,13 +19,9 @@ def introduction():
          # Code requesting User chooses how hard the game should be
          print("There are three difficulty levels you can choose from (Easy, Medium and Hard), or you can have a mix of them all.")
          level_choice = input("Please choose a level now (Easy, Medium, Hard or Mix):\n").lower()
-         return level_choice
+         return level_choice.lower()
     else:
         introduction()
-
-# Calls the introduction fn, and assigns level a value to be used in the next fn (word_choice)
-###level = introduction()
-###print(level)
 
 
 # Fn takes level's value from prev. fn & uses it to open the relevant text file, then chooses a random word to use.
@@ -36,27 +30,18 @@ def word_choice(level):
         word_selector = easy.read().split("\n")
     elif "medium" in level:
         word_selector = medium.read().split("\n")
+    elif "hard" in level:
+        word_selector = hard.read().split("\n")
+    elif "mix" in level:
+        word_selector = mix.read().split("\n")
     # Assigns the word chosen by random.choice to the variable "word"
     # "word" is the word the User will try and guess
     game_word = random.choice(word_selector)
     return game_word
     #print(word)
 
-# Calls the word_choice fn, and assigns game_word a value to be used in the next fn (hide_word)
-###game_word = word_choice(level)
-###print(game_word)
 
 # Fn to go through letters in game_word and replace each with "_"        
-###def hide_word(game_word):
-###    blank = "_" * len(game_word)
-###    return blank
-
-# Calls the hide_word fn, and assigns hidden_word the game_word with letters replaced as blanks
-###hidden_word = hide_word(game_word)
-###print(hidden_word)
-
-game_word = "colder"
-
 def check_guess(game_word, user_guesses):
     word_with_guesses = ""
     for letter in game_word:
@@ -64,10 +49,10 @@ def check_guess(game_word, user_guesses):
             word_with_guesses += letter
         else:
             word_with_guesses += "_"
-    #print(word_with_blanks)
-    #print(f"You've guessed these letters: {user_guesses}")
     return word_with_guesses
 
+# Fn that's called if User's guess isn't right
+# Checks chances and displays the apropriate stick figure
 def hanged_man(chances):
     hanged_man = [
         r"""
@@ -172,7 +157,6 @@ def hanged_man(chances):
     print(hanged_man[chances])
 
 
-
 ###FUNCTIONS:
 ###introduction() - Gets user's name and level desired
 ###word_choice() - Takes the result from introduction() and chooses a word from the corresponding txt file. Then assigns it the variable name "game_word"
@@ -196,6 +180,7 @@ def game_start():
     while chances > 0:
         user_guess = input("Choose a letter you think might be in the word: ").lower()
         # Nested statment to check if input is valid
+        print("****************************")
         if user_guess.isalpha():
             user_guesses.append(user_guess)
             if user_guess in word:
@@ -218,7 +203,8 @@ def game_start():
         word_with_guesses = check_guess(word, user_guesses)
 
         # Prompts User for another guess 
-        print(f"Have another go: \n{word_with_guesses}")
+        if chances != 0:
+            print(f"Have another go: \n{word_with_guesses}\n")
 
         # Displays letters User has guessed so far to them
         print(f"You've guessed these letters so far: {user_guesses}")
